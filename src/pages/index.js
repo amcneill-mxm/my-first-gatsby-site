a51ce19aa11e78fdbe6b4a76170af6177066b35c
Alexis McNeill <amcneill@166.168.mxmsite.com>
2022-01-10 14:27:36 -0500

    Finished Gatsby Tutorial Part 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import * as React from "react"
 import {Link} from "gatsby"
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/clifford.jpg"
+      />
     </Layout>
   )
 }
